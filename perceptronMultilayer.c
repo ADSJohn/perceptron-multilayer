caee62c5dfcd266a0d1c4b9dab2155dba926d701
ADSJohn <progjohn@outlook.com>
2025-09-09 22:48:54 -0300

    ajuste na leitura do arquivo

--- perceptronMultilayer.c
+++ perceptronMultilayer.c
@@ -3,8 +3,8 @@
 #include <math.h>
 #include <time.h>
 
-#define N_DADOS 1
-const int camadas[] = {7, 4, 1};
+#define N_DADOS 5
+#define N_COLS 6
 
 float **inicializaPesos(float **pesos, int lin, int col)
 {
@@ -67,7 +67,7 @@ void lerArquivo(float **dados)
     }
     for (i = 0; i < N_DADOS; i++)
     {
-        for (j = 0; j < camadas[0]; j++)
+        for (j = 0; j < N_COLS; j++)
         {
             fscanf(file, "%f", &dados[i][j]);
         }
@@ -149,52 +149,56 @@ void atualizaPesos(float **pesos, float *entradas, float *delta, float taxaApren
 int main()
 {
 
-    srand(time(NULL));
-
-    float **pesos1;
-    float **pesos2;
-    float **entradas;
-    float bias[2] = {0.5, 0.3};
-    float *saidaEsperada = malloc(1 * sizeof(float));
-    saidaEsperada[0] = 0.95;
-    float *uh = malloc(sizeof(float) * camadas[1]);
-    float *uo = malloc(camadas[2] * sizeof(float));
-    int epoca = 0;
-    float *deltao = malloc(camadas[2] * sizeof(float));
-    float *deltah = malloc(camadas[1] * sizeof(float));
-
-    pesos1 = inicializaPesos(pesos1, camadas[1], camadas[0]);
-    pesos2 = inicializaPesos(pesos2, camadas[2], camadas[1]);
-
-    entradas = alocaMatriz(entradas, 1, 7);
+    float **entradas = alocaMatriz(NULL, N_DADOS, N_COLS);
     lerArquivo(entradas);
+    imprimeMatriz(entradas, N_DADOS, N_COLS);
 
-    while (epoca < 1000)
-    {
-        imprimeMatriz(pesos1, camadas[1], camadas[0]);
-        printf("\n");
-        imprimeMatriz(pesos2, camadas[2], camadas[1]);
-        uh = propagacao(entradas[0], pesos1, bias[0], camadas[1], camadas[0], uh);
-        uo = propagacao(uh, pesos2, bias[1], camadas[2], camadas[1], uo);
-        deltao = deltaO(saidaEsperada, uo, camadas[2], deltao);
-        printf("Aqui...");
-        deltah = deltaH(pesos2, deltao, uh, camadas[1], camadas[2], deltah);
+    // srand(time(NULL));
 
-        atualizaPesos(pesos2, uh, deltao, 0.5, camadas[2], camadas[1]);
-        atualizaPesos(pesos1, entradas[0], deltah, 0.5, camadas[1], camadas[0]);
+    // float **pesos1;
+    // float **pesos2;
+    // float **entradas;
+    // float bias[2] = {0.5, 0.3};
+    // float *saidaEsperada = malloc(1 * sizeof(float));
+    // saidaEsperada[0] = 0.95;
+    // float *uh = malloc(sizeof(float) * camadas[1]);
+    // float *uo = malloc(camadas[2] * sizeof(float));
+    // int epoca = 0;
+    // float *deltao = malloc(camadas[2] * sizeof(float));
+    // float *deltah = malloc(camadas[1] * sizeof(float));
 
-        printf("Epoca: %d\n", epoca);
-        printf("Erro: %f\n", (saidaEsperada[0] - uo[0]));
-        printf("Saida: %f\n", uo[0]);
-        epoca++;
-    }
+    // pesos1 = inicializaPesos(pesos1, camadas[1], camadas[0]);
+    // pesos2 = inicializaPesos(pesos2, camadas[2], camadas[1]);
 
-    liberaMatriz(pesos1, camadas[1]);
-    liberaMatriz(pesos2, camadas[2]);
-    free(saidaEsperada);
-    liberaMatriz(entradas, 1);
-    free(uh);
-    free(uo);
+    // entradas = alocaMatriz(entradas, 1, 7);
+    // lerArquivo(entradas);
+
+    // while (epoca < 1000)
+    // {
+    //     imprimeMatriz(pesos1, camadas[1], camadas[0]);
+    //     printf("\n");
+    //     imprimeMatriz(pesos2, camadas[2], camadas[1]);
+    //     uh = propagacao(entradas[0], pesos1, bias[0], camadas[1], camadas[0], uh);
+    //     uo = propagacao(uh, pesos2, bias[1], camadas[2], camadas[1], uo);
+    //     deltao = deltaO(saidaEsperada, uo, camadas[2], deltao);
+    //     printf("Aqui...");
+    //     deltah = deltaH(pesos2, deltao, uh, camadas[1], camadas[2], deltah);
+
+    //     atualizaPesos(pesos2, uh, deltao, 0.5, camadas[2], camadas[1]);
+    //     atualizaPesos(pesos1, entradas[0], deltah, 0.5, camadas[1], camadas[0]);
+
+    //     printf("Epoca: %d\n", epoca);
+    //     printf("Erro: %f\n", (saidaEsperada[0] - uo[0]));
+    //     printf("Saida: %f\n", uo[0]);
+    //     epoca++;
+    // }
+
+    // liberaMatriz(pesos1, camadas[1]);
+    // liberaMatriz(pesos2, camadas[2]);
+    // free(saidaEsperada);
+    // liberaMatriz(entradas, 1);
+    // free(uh);
+    // free(uo);
 
     return 0;
 }
